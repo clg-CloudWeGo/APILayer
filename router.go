47aadb41bbd2fb93d7103c9e86962002e80f8d58
Loganloxsins <1253764570@qq.com>
2023-07-30 02:25:18 +0800

    请看修改内容和群内图片

--- router.go
+++ router.go
@@ -10,6 +10,6 @@ import (
 // customizeRegister registers customize routers.
 func customizedRegister(r *server.Hertz) {
 	r.GET("/ping", handler.Ping)
-
+	r.POST("/:service/:method", handler.SolveUrl)
 	// your code ...
 }
